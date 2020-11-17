2e42d133a34b9e90a69feaf3b7576bb5832cc492
Dameon Wilson <thebrowncoder@gmail.com>
2020-11-17 09:45:05 -0500

    Added function that shows your clicks in the map

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-// var marker = L.marker = L.marker([51.5, -0.09]).addTo(mymap);
+var popup = L.popup();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,3 +9,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidGhlYnJvd25jb2RlciIsImEiOiJja2hrcHU5ZW0weHpzMnNzZHpzMndod2lzIn0.V7fT7FU9IeBJvvrMxFydvQ'
 }).addTo(mymap);
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
