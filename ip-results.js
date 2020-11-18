5a2c188a838569c27b38bfd9c192bc1985c24671
Dameon Wilson <thebrowncoder@gmail.com>
2020-11-18 00:02:34 -0500

    ......

--- ip-results.js
+++ ip-results.js
@@ -1,6 +1,6 @@
 'use strict';
 const geo_uri = "https://geo.ipify.org/api/";
-const secret_key = "at_9pqXfbX3e4qNlO8sVBSa2VL53MkEG";
+const secret_key = "Enter Your API Key";
 const geo_version = "v1";
 const default_ip = "8.8.4.4";
 
@@ -23,9 +23,6 @@ function getIP () {
         .then(results => results.json())
         .then(data => {
             ip_location.innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
-            // ip_city.innerHTML = data.location.city
-            // ip_region.innerHTML = data.location.region
-            // ip_postal_code.innerHTML = data.location.postalCode
             ip_timezone.innerHTML = `UTC ${data.location.timezone}`
             ip_isp.innerHTML = data.isp
         })
